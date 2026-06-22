9b05a9b50df3c2698d675cbc623db57809b512b5
goofy-coder-1 <ujjwolrana2332@gmail.com>
2026-06-22 13:51:12 +0545

    wassuppp

--- GYM/registration.py
+++ GYM/registration.py
@@ -38,4 +38,58 @@ class userBase:
         return False
     
     @classmethod
-    def save_to_csv()
+    def save_to_csv(cls, account):
+        file_exits = os.path.exists(cls.CSV_FILE)
+
+        with open(cls.CSV_FILE, mode="a", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+
+            if not file_exits:
+                writer.writerow(["Name", "Address", "Age", "Height", "Weight"])
+
+            writer.writerow([
+                account.name,
+                account.address,
+                account.age,
+                account.height,
+                account.weight
+            ])
+    
+    @classmethod
+    def register_new_user(cls):
+        try:
+            name = input("Enter your name: ")
+            address = input("Enter the address: ")
+            age = input("Enter your age: ")
+            
+            if cls.existence_checking(name, address, age):
+                print("\nRegistration Failed: Data already exists in the system!")
+                return None
+
+            height = int(input("Enter your height in cm: "))
+            weight = int(input("Weight in Kg: "))
+
+           
+            user_account = Userdetail(name, address, age, height, weight)
+            
+            
+            user_account.displayDetails()
+            
+            
+            confirmation = input("Do you want to save these details? (yes/no): ").strip().lower()
+            
+            if confirmation in ["yes", "y"]:
+                cls.save_to_csv(user_account)
+                print("Account successfully created and recorded in database!")
+                return user_account
+            else:
+                print("Registration cancelled by user. Data was not saved.")
+                return None
+
+        except ValueError:
+            print("\nInput error detected! Please ensure your height and weight are numbers.\n")
+            return None
+
+
+if __name__ == "__main__":
+    active_account = userBase.register_new_user()
