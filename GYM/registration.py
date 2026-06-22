51f0b3c947d84d4ad45cad303b2fb3751cc4a0c6
goofy-coder-1 <ujjwolrana2332@gmail.com>
2026-06-22 13:33:21 +0545

    don't

---
+++ GYM/registration.py
@@ -0,0 +1,7 @@
+class Userdetail:
+     def __init__(self, name, address, age, pin, capital):
+        self.name = name
+        self.address = address
+        self.age = age
+        self.pin = pin
+        self.capital = capital
