9858f13f852002c29052be63a755548746d843bd
goofy-coder-1 <ujjwolrana2332@gmail.com>
2026-07-01 15:46:44 +0545

    some changes

--- Supermarket/Owner/structure.py
+++ Supermarket/Owner/structure.py
@@ -1,18 +1,16 @@
 from datetime import datetime
 
 class ItemStructure:
-    def __init__(self, name, category, price, added_date):
+    def __init__(self, name:str, category:str, price:int, added_date:str = None):
            self.name = name
            self.category = category
            self.price = price
-           self.added_date = datetime.now("%Y-%m-%d %H-%M-%S")
+           self.added_date = added_date or datetime.now().strftime("%Y-%m-%d %H-%M-%S")
 
     def DisplayDetails(self):
           print("\n"+"="*40)
-          print("="*20+"Details Below"+"="*20)
+          print("="*13+"Details Below"+"="*14)
           print(f"Name         :   {self.name}")
           print(f"Category     :   {self.category}")
           print(f"Price        :   {self.price}")
           print(f"Added Date   :   {self.added_date}")
-
-
