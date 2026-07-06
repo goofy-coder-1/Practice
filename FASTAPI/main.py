bae27ba7af9204c3aeb567af7b2bc8630c8e3c35
goofy-coder-1 <ujjwolrana2332@gmail.com>
2026-07-06 16:55:52 +0545

    new things

--- FASTAPI/main.py
+++ FASTAPI/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
    return {"Message" : "Hello world"}
+
+@app.get("/items/item_id")
+async def read_item():
+   return {"Message" : "Aahile lai chahi khali chha hai"}
