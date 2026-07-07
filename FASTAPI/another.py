0392bb1689b51fd9348da4859baeddb8f5e8f116
goofy-coder-1 <ujjwolrana2332@gmail.com>
2026-07-07 16:09:02 +0545

    new things

--- FASTAPI/another.py
+++ FASTAPI/another.py
@@ -8,3 +8,21 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
+@app.get("/calculate")
+async def calculator(a: int, b: int, op: str):
+    match op:
+        case '+':
+            result = a + b
+        case '-':
+            result = a - b
+        case '*':
+            result = a * b
+        case '/':
+            if b == 0:
+                return {"error": "Cannot divide by zero"}
+            result = a / b
+        case _:
+            return {"error": "Invalid operation"}
+            
+    return {"result": result}
