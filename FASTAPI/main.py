9c7534eee9e2fd2e6cc9e4850e71a14aecdede31
goofy-coder-1 <ujjwolrana2332@gmail.com>
2026-07-07 15:50:30 +0545

    new things

--- FASTAPI/main.py
+++ FASTAPI/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -9,3 +10,18 @@ async def root():
 @app.get("/item/{item_id}")
 async def read_item(item_id: int):
    return {"Item Id" : item_id}
+
+class ModalName(str, Enum):
+   hello = "hello"
+   lenet = "lenet"
+   anything = "anything"
+
+@app.get("/users/{model_name}")
+async def get_model(model_name: ModalName):
+   if model_name is ModalName.anything:
+      return {"Modal name": model_name, "Message": "Wassuppp"}
+   
+   if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+   return {"model_name": model_name, "message": "Have some residuals"}
